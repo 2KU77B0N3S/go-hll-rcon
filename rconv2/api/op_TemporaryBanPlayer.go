b1573aa36ddf20654d4ea3fa66f472b624d81893
2KU77B0N3S <thomasfellnerat@gmail.com>
2025-06-26 15:56:08 +0200

    Delete rconv2/api/op_TemporaryBanPlayer.go

--- rconv2/api/op_TemporaryBanPlayer.go
+++
@@ -1,8 +0,0 @@
-package api
-
-type TemporaryBanPlayer struct {
-	PlayerId  string `json:"playerId"`
-	Duration  int32  `json:"duration"`
-	Reason    string `json:"reason"`
-	AdminName string `json:"adminName"`
-}
